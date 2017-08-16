ada05a5f9fe8c22c666ea4fcf4c8d2ad1ee61c10
AnnaYatsun1 <anny0-2011@mail.ru>
2017-08-16 11:53:01 +0300

    next commit to Hellow world

--- chapter_001/src/mainjava/job4j/Calculate.java
+++ chapter_001/src/mainjava/job4j/Calculate.java
@@ -1,5 +1,13 @@
 package ru.job4j;
+/**
+* Package for calculate task.
+*
+* @author Yatsun (anny0-2011@mail.ru)
+* @version $Id$
+* @since 0.1
+*/
 public class Calculate { 
+	
 	public static void main(String[] args) {
 		System.out.println("Hellow world");
 	}
