8add8036af738cf9a4cbffc77a5fa455f61388d2
AnnaYatsun1 <anny0-2011@mail.ru>
2017-09-03 15:00:20 +0300

    Max 2 чисел

---
+++ chapter_001/src/main/java/ru/job4j/max/Max.java
@@ -0,0 +1,26 @@
+package ru.job4j.max;
+
+/**
+ * Max.
+ *
+ * @author Anna Yatsun (anny0-2011@mail.ru)
+ * @version $Id$
+ * @since 0.1
+
+/**
+ * Created by Анна on 23.08.2017.
+ */
+
+public class Max {
+
+
+    public int max(int first, int second) {
+
+
+        return first > second ? first : second;
+
+
+    }
+
+}
+  
