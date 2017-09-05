faa22552c16bc2d9157bdc8ee397d3600749a3d5
AnnaYatsun1 <anny0-2011@mail.ru>
2017-09-05 11:27:59 +0300

    Изминения в методе Max. Мак из 3х чисел

--- chapter_001/src/main/java/ru/job4j/max/Max.java
+++ chapter_001/src/main/java/ru/job4j/max/Max.java
@@ -13,16 +13,11 @@ package ru.job4j.max;
 
 public class Max {
 
+    public int max(int first, int second, int third){
 
-    public int max(int first, int second, int third ) {
-     
-        return first > second ? first : second;
-        return Math.max(first, Math.max(second, third));
-
-
+        //int smallest =Math.min(a, Math.min(c, b));
+        return Math.max(first, Math.max(third, second));
     }
 
-    }
-
-
+}
   
