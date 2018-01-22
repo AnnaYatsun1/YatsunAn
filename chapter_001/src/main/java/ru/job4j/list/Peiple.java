2b56f83207b1529cede09034b86a30131eb15de3
Сакура-тян <https://github.com/AnnaYatsun1/YatsunAn.git>
2018-01-22 16:34:02 +0300

    HashMap Node

--- chapter_001/src/main/java/ru/job4j/list/Peiple.java
+++ chapter_001/src/main/java/ru/job4j/list/Peiple.java
@@ -5,9 +5,12 @@ import java.text.SimpleDateFormat;
 import java.util.*;
 
 public class Peiple {
+
     String name;
     int children;
     Calendar birthday = Calendar.getInstance();
+
+
 
 
     public Peiple(String name, int children, int month, int day, int year){
@@ -18,12 +21,27 @@ public class Peiple {
         birthday.set(Calendar.DAY_OF_MONTH, day);
         birthday.set(Calendar.YEAR, year);
     }
+
     public void setBirthday(int month, int day, int year, Calendar birthday) throws ParseException {
         birthday = Calendar.getInstance();
         birthday.set(Calendar.MONTH, month);
         birthday.set(Calendar.DAY_OF_MONTH, day);
         birthday.set(Calendar.YEAR, year);
         this.birthday = birthday;
+    }
+    @Override
+    public int hashCode() {
+
+        return Objects.hash(name, children, birthday);
+    }
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Peiple peiple = (Peiple) o;
+        return children == peiple.children &&
+                Objects.equals(name, peiple.name) &&
+                Objects.equals(birthday, peiple.birthday);
     }
     public static void main(String[] args) {
         Peiple peiple = new Peiple("Ahjsdfh", 2, 5, 18, 1990);
