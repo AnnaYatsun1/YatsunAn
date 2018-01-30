1f2ebc5926a2e1ce3921478c9043f6fa900dafa2
Сакура-тян <https://github.com/AnnaYatsun1/YatsunAn.git>
2018-01-30 13:53:28 +0300

    Исправила

--- chapter_001/src/main/java/ru/job4j/list/NodeForLinked.java
+++ chapter_001/src/main/java/ru/job4j/list/NodeForLinked.java
@@ -6,4 +6,35 @@ import java.util.Arrays;
 
 public class NodeForLinked<E>  {
 
+
+    static class  Node<K,V>{
+
+        private K key;
+
+        private NodeForMap.Node next;
+
+        public Node(){}
+
+        public Node(K key, V value){
+            this.key = key;
+
+        }
+
+        public K getKey() {
+            return key;
+        }
+
+        public void setKey(K key) {
+            this.key = key;
+        }
+
+
+        public NodeForMap.Node getNext() {
+            return next;
+        }
+
+        public void setNext(NodeForMap.Node next) {
+            this.next = next;
+        }
+    }
 }
