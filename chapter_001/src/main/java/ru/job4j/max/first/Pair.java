366880853600c994ee97039c430b6e7476f7792b
YatsunAnna1 <anny0-2011@mail.ru>
2018-06-21 13:06:41 +0200

    file with concon words

---
+++ chapter_001/src/main/java/ru/job4j/max/first/Pair.java
@@ -0,0 +1,22 @@
+package ru.job4j.max.first;
+
+public class Pair<T> {
+
+    private T first;
+    private T second;
+
+    public Pair(T first, T second) {
+        this.first = first;
+        this.second = second;
+    }
+
+    // return first element
+    public T first() {
+        return first;
+    }
+
+    // return second element
+    public T second() {
+        return second;
+    }
+}
